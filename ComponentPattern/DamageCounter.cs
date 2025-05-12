ba52f4a9f681b1fa58ba16f1d90636920a6e8ccd
emje-dania <emje7@student.eadania.dk>
2025-05-12 12:21:09 +0200

    created DamageCounter
    the health thinga-ma-do

---
+++ ComponentPattern/DamageCounter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaiju.ComponentPattern
+{
+    internal class DamageCounter : Component
+    {
+        private SpriteFont hudFont;
+        private string text;
+
+        public DamageCounter(GameObject gameObject) : base(gameObject)
+        {
+        }
+    }
+}
