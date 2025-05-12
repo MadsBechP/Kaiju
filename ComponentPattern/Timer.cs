3fe429855b7fb89b7ed3b083ca003f54309b858f
emje-dania <emje7@student.eadania.dk>
2025-05-12 12:13:52 +0200

    timer is working

--- ComponentPattern/Timer.cs
+++ ComponentPattern/Timer.cs
@@ -11,8 +11,9 @@ namespace Kaiju.ComponentPattern
     internal class Timer : Component
     {
         private SpriteFont hudFont;
-        private string text = "02:00";
+        private string text;
         private Vector2 position = new Vector2(GameWorld.Instance.Graphics.PreferredBackBufferWidth / 2, 20);
+        private float timeLeft = 120f;
         public Timer(GameObject gameObject) : base(gameObject)
         {
         }
@@ -25,6 +26,16 @@ namespace Kaiju.ComponentPattern
         public override void Update()
         {
             base.Update();
+            if (timeLeft > 0f)
+            {
+                timeLeft -= GameWorld.Instance.DeltaTime;
+                if (timeLeft < 0f)
+                {
+                    timeLeft = 0f;
+                }
+                TimeSpan time = TimeSpan.FromSeconds(timeLeft);
+                text = $"{time.Minutes:D2}:{time.Seconds:D2}";
+            }
         }
         public override void Draw(SpriteBatch spriteBatch)
         {
