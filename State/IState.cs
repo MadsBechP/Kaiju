2f34a1e84de0b310beacdbe8b6cde1af2dde64c4
jukr5-eadania <jukr5@student.eadania.dk>
2025-05-14 13:11:43 +0200

    State Pattern added
    Pretty much all the logic comes when adding the AI

---
+++ State/IState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaiju.State
+{
+    public interface IState<T>
+    {
+        void Enter(Task parrent);
+        void Exit();
+        void Execute();
+    }
+}
---
+++ State/MoveState.cs
@@ -0,0 +1,28 @@
+﻿using Kaiju.ComponentPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaiju.State
+{
+    public class MoveState : IState<Player>
+    {
+
+        public void Enter(Task parrent)
+        {
+            
+        }
+
+        public void Execute()
+        {
+            
+        }
+
+        public void Exit()
+        {
+            
+        }
+    }
+}
