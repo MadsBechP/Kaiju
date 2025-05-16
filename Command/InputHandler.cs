e41f40605ee2bc7c1f9a1c524cb9c7f1a8ddd887
jukr5-eadania <jukr5@student.eadania.dk>
2025-05-16 13:51:59 +0200

    Inputhandler updated with controller input
    needs to be tested and updated to support two players

--- Command/InputHandler.cs
+++ Command/InputHandler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Input;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 
 namespace Kaiju.Command
@@ -23,19 +24,40 @@ namespace Kaiju.Command
 
         }
 
+        //Keyboard commands
         private Dictionary<Keys, ICommand> keybindsUpdate = new Dictionary<Keys, ICommand>();
         private Dictionary<Keys, ICommand> keybindsButtonDown = new Dictionary<Keys, ICommand>();
         private KeyboardState previousKeyState;
+
+        //GamePad commands
+        private Dictionary<Buttons, ICommand> buttonbindsUpdate = new();
+        private Dictionary<Buttons, ICommand> buttonbindsButtonDown = new();
+        private GamePadState previousButtonState;
+        private PlayerIndex playerIndex = PlayerIndex.One;
+
         public void AddUpdateCommand(Keys inputKey, ICommand command)
         {
             keybindsUpdate.Add(inputKey, command);
         }
+
         public void AddButtonDownCommand(Keys inputKey, ICommand command)
         {
             keybindsButtonDown.Add(inputKey, command);
         }
+
+        public void AddUpdateCommand(Buttons inputButton, ICommand command)
+        {
+            buttonbindsUpdate.Add(inputButton, command);
+        }
+
+        public void AddButtonDownCommand(Buttons inputButton, ICommand command)
+        {
+            buttonbindsButtonDown.Add(inputButton, command);
+        }
+
         public void Execute()
         {
+            //Keyboard input
             KeyboardState keyState = Keyboard.GetState();
 
             foreach (var pressedKey in keyState.GetPressedKeys())
@@ -54,6 +76,29 @@ namespace Kaiju.Command
             }
             previousKeyState = keyState;
 
+            //Gamepad input
+            GamePadState gamePadState = GamePad.GetState(PlayerIndex.One);
+            if (gamePadState.IsConnected)
+            {
+                foreach (Buttons button in System.Enum.GetValues(typeof(Buttons)))
+                {
+                    if (gamePadState.IsButtonDown(button))
+                    {
+                        if (buttonbindsUpdate.TryGetValue(button, out var cmd))
+                        {
+                            cmd.Execute();
+                        }
+                        if (!previousButtonState.IsButtonDown(button))
+                        {
+                            if (buttonbindsButtonDown.TryGetValue(button, out var cmdBd))
+                            {
+                                cmdBd.Execute();
+                            }
+                        }
+                    }
+                }
+                previousButtonState = gamePadState;
+            }
         }
     }
 }
