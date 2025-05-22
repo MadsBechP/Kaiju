4421eee5c4149100f5dcd97357ffb4ef7469e6b1
emje-dania <emje7@student.eadania.dk>
2025-05-22 11:22:34 +0200

    cleaning from merge

--- GameWorld.cs
+++ GameWorld.cs
@@ -72,45 +72,45 @@ namespace Kaiju
 
         protected override void Initialize()
         {
-            player1Go = new GameObject();
-            player1 = player1Go.AddComponent<Player>();
-            player1.InputType = InputType.Keyboard;
-            player1.GamePadIndex = PlayerIndex.One;
-            player1Go.AddComponent<SpriteRenderer>();
-            player1Go.AddComponent<Collider>();
-            player1.collider = player1Go.AddComponent<Collider>(player1);
-            player1Go.AddComponent<Animator>();
-            player1.chr = player1Go.AddComponent<Godzilla>();
-            gameObjects.Add(player1Go);
+            //player1Go = new GameObject();
+            //player1 = player1Go.AddComponent<Player>();
+            //player1.InputType = InputType.Keyboard;
+            //player1.GamePadIndex = PlayerIndex.One;
+            //player1Go.AddComponent<SpriteRenderer>();
+            //player1Go.AddComponent<Collider>();
+            //player1.collider = player1Go.AddComponent<Collider>(player1);
+            //player1Go.AddComponent<Animator>();
+            //player1.chr = player1Go.AddComponent<Godzilla>();
+            //gameObjects.Add(player1Go);
 
-            player2Go = new GameObject();
-            player2 = player2Go.AddComponent<Player>();
-            player2.InputType = InputType.Keyboard;
-            player2.GamePadIndex = PlayerIndex.Two;
-            player2Go.AddComponent<SpriteRenderer>();
-            player2Go.AddComponent<Collider>();
-            player2.collider = player2Go.AddComponent<Collider>(player2);
-            player2Go.AddComponent<Animator>();
-            player2.chr = player2Go.AddComponent<Gigan>();
-            gameObjects.Add(player2Go);
+            //player2Go = new GameObject();
+            //player2 = player2Go.AddComponent<Player>();
+            //player2.InputType = InputType.Keyboard;
+            //player2.GamePadIndex = PlayerIndex.Two;
+            //player2Go.AddComponent<SpriteRenderer>();
+            //player2Go.AddComponent<Collider>();
+            //player2.collider = player2Go.AddComponent<Collider>(player2);
+            //player2Go.AddComponent<Animator>();
+            //player2.chr = player2Go.AddComponent<Gigan>();
+            //gameObjects.Add(player2Go);
 
-            stageGo = new GameObject();
-            stageGo.AddComponent<SpriteRenderer>();
-            stageGo.AddComponent<Collider>();
-            stageGo.AddComponent<Stage>();
-            gameObjects.Add(stageGo);
+            //stageGo = new GameObject();
+            //stageGo.AddComponent<SpriteRenderer>();
+            //stageGo.AddComponent<Collider>();
+            //stageGo.AddComponent<Stage>();
+            //gameObjects.Add(stageGo);
 
 
-            GameObject timerGo = new GameObject();
-            timerGo.AddComponent<Timer>();
+            //GameObject timerGo = new GameObject();
+            //timerGo.AddComponent<Timer>();
 
-            UIObjects.Add(timerGo);
-            timerGo.Awake();
+            //UIObjects.Add(timerGo);
+            //timerGo.Awake();
 
-            foreach (var gameObject in gameObjects)
-            {
-                gameObject.Awake();
-            }
+            //foreach (var gameObject in gameObjects)
+            //{
+            //    gameObject.Awake();
+            //}
 
             currentState = new BattleState(this); // starter scenen
 
@@ -120,75 +120,75 @@ namespace Kaiju
         protected override void LoadContent()
         {
             background = Content.Load<Texture2D>("City");
-            Texture2D player1Profile = Content.Load<Texture2D>("GZProfile");
-            Texture2D player2Profile = Content.Load<Texture2D>("GZProfile");
-            string name1 = "null";
-            string name2 = "null";
-            switch (player1.chr)
-            {
-                case Godzilla:
-                    {
-                        player1Profile = Content.Load<Texture2D>("GZProfile");
-                        name1 = "Godzilla";
-                        break;
-                    }
-                case Gigan:
-                    {
-                        player1Profile = Content.Load<Texture2D>("GiganProfile");
-                        name1 = "Gigan";
-                        break;
-                    }
-            }
-            switch (player2.chr)
-            {
-                case Godzilla:
-                    {
-                        player2Profile = Content.Load<Texture2D>("GZProfile");
-                        name2 = "Godzilla";
-                        break;
-                    }
-                case Gigan:
-                    {
-                        player2Profile = Content.Load<Texture2D>("GiganProfile");
-                        name2 = "Gigan";
-                        break;
-                    }
-            }
+            //Texture2D player1Profile = Content.Load<Texture2D>("GZProfile");
+            //Texture2D player2Profile = Content.Load<Texture2D>("GZProfile");
+            //string name1 = "null";
+            //string name2 = "null";
+            //switch (player1.chr)
+            //{
+            //    case Godzilla:
+            //        {
+            //            player1Profile = Content.Load<Texture2D>("GZProfile");
+            //            name1 = "Godzilla";
+            //            break;
+            //        }
+            //    case Gigan:
+            //        {
+            //            player1Profile = Content.Load<Texture2D>("GiganProfile");
+            //            name1 = "Gigan";
+            //            break;
+            //        }
+            //}
+            //switch (player2.chr)
+            //{
+            //    case Godzilla:
+            //        {
+            //            player2Profile = Content.Load<Texture2D>("GZProfile");
+            //            name2 = "Godzilla";
+            //            break;
+            //        }
+            //    case Gigan:
+            //        {
+            //            player2Profile = Content.Load<Texture2D>("GiganProfile");
+            //            name2 = "Gigan";
+            //            break;
+            //        }
+            //}
 
 
 
 
-            GameObject player1DamageMeterGo = new GameObject();
-            var playerDamageMeter = player1DamageMeterGo.AddComponent<DamageMeter>();
-            playerDamageMeter.Setup(
-                name1,
-                player1Profile,
-                new Vector2((Graphics.PreferredBackBufferWidth / 2) - 750, Graphics.PreferredBackBufferHeight - 185), // damageFontPos
-                new Vector2((Graphics.PreferredBackBufferWidth / 2) - 735, Graphics.PreferredBackBufferHeight - 80), // namePos
-                new Vector2((Graphics.PreferredBackBufferWidth / 2) - 1000, Graphics.PreferredBackBufferHeight - 250), // hudPos
-                new Vector2((Graphics.PreferredBackBufferWidth / 2) - 950, Graphics.PreferredBackBufferHeight - 200) // profilePos
-               );
+            //GameObject player1DamageMeterGo = new GameObject();
+            //var playerDamageMeter = player1DamageMeterGo.AddComponent<DamageMeter>();
+            //playerDamageMeter.Setup(
+            //    name1,
+            //    player1Profile,
+            //    new Vector2((Graphics.PreferredBackBufferWidth / 2) - 750, Graphics.PreferredBackBufferHeight - 185), // damageFontPos
+            //    new Vector2((Graphics.PreferredBackBufferWidth / 2) - 735, Graphics.PreferredBackBufferHeight - 80), // namePos
+            //    new Vector2((Graphics.PreferredBackBufferWidth / 2) - 1000, Graphics.PreferredBackBufferHeight - 250), // hudPos
+            //    new Vector2((Graphics.PreferredBackBufferWidth / 2) - 950, Graphics.PreferredBackBufferHeight - 200) // profilePos
+            //   );
 
-            GameObject player2DamageMeterGo = new GameObject();
-            var player2DamageMeter = player2DamageMeterGo.AddComponent<DamageMeter>();
-            player2DamageMeter.Setup(
-                name2,
-                player2Profile,
-                new Vector2((Graphics.PreferredBackBufferWidth / 2) + 790, Graphics.PreferredBackBufferHeight - 185), // damageFontPos
-                new Vector2((Graphics.PreferredBackBufferWidth / 2) + 780, Graphics.PreferredBackBufferHeight - 80), // namePos
-                new Vector2((Graphics.PreferredBackBufferWidth / 2) + 550, Graphics.PreferredBackBufferHeight - 250), // hudPos
-                new Vector2((Graphics.PreferredBackBufferWidth / 2) + 610, Graphics.PreferredBackBufferHeight - 200) // profilePos
-               );
+            //GameObject player2DamageMeterGo = new GameObject();
+            //var player2DamageMeter = player2DamageMeterGo.AddComponent<DamageMeter>();
+            //player2DamageMeter.Setup(
+            //    name2,
+            //    player2Profile,
+            //    new Vector2((Graphics.PreferredBackBufferWidth / 2) + 790, Graphics.PreferredBackBufferHeight - 185), // damageFontPos
+            //    new Vector2((Graphics.PreferredBackBufferWidth / 2) + 780, Graphics.PreferredBackBufferHeight - 80), // namePos
+            //    new Vector2((Graphics.PreferredBackBufferWidth / 2) + 550, Graphics.PreferredBackBufferHeight - 250), // hudPos
+            //    new Vector2((Graphics.PreferredBackBufferWidth / 2) + 610, Graphics.PreferredBackBufferHeight - 200) // profilePos
+            //   );
 
-            UIObjects.Add(player1DamageMeterGo);
-            UIObjects.Add(player2DamageMeterGo);
+            //UIObjects.Add(player1DamageMeterGo);
+            //UIObjects.Add(player2DamageMeterGo);
 
-            player1DamageMeterGo.Awake();
-            player2DamageMeterGo.Awake();
+            //player1DamageMeterGo.Awake();
+            //player2DamageMeterGo.Awake();
 
 
-            playerDamageMeter.SetSubject(player1);
-            player2DamageMeter.SetSubject(player2);
+            //playerDamageMeter.SetSubject(player1);
+            //player2DamageMeter.SetSubject(player2);
 
             //AIDamageMeter.Updated();
             //playerDamageMeter.Updated();
